ea3e5ef7b01cd6ad99884472e3eaeaf1aedcbceb
justjo3l <comedyandcomputers@gmail.com>
2025-05-22 02:09:57 +1000

    Initial commit

---
+++ ecosense-web/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import PlaceholderPage from './pages/PlaceholderPage'
+
+function App() {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <PlaceholderPage />
+    </div>
+  )
+}
+
+export default App
